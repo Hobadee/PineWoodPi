f6d9236d6c2949c1a9d1a607d2e9c3e9a3ecfb9c
Eric Kincl <eric@kincl.net>
2023-01-10 21:53:48 -0800

    Working test

--- Testing.py
+++ Testing.py
@@ -3,17 +3,18 @@
 import RPi.GPIO as GPIO
 import time
 import board
-from gpiozero import Button
+from gpiozero import Button, DigitalInputDevice
 
 GPIO.setmode(GPIO.BCM)
 
-LANE_1_SENSE=0
-LANE_2_SENSE=0
-LANE_3_SENSE=12
-LANE_4_SENSE=23
+LANE_1_SENSE=22
+LANE_2_SENSE=23
+LANE_3_SENSE=21
+LANE_4_SENSE=20
 
-LANE_3 = Button(board.D12.id, False)
-LANE_4 = Button(board.D23.id, False)
+LANE_3 = DigitalInputDevice(LANE_3_SENSE, True)
+#LANE_4 = Button(board.D23.id, True)
+LANE_4 = DigitalInputDevice(LANE_4_SENSE, True)
 
 #LANE_1_LIGHT=
 
@@ -24,20 +25,23 @@ GPIO.setup(LANE_4_SENSE, GPIO.IN)
 
 #GPIO.setup(LANE_1_LIGHT, GPIO.OUT)
 
+L3STS="False"
+L4STS="False"
 
 while True:
   #if GPIO.input(LANE_3_SENSE) == 1:
   if LANE_3.value == 1:
     #GPIO.output(LANE_1_LIGHT, GPIO.LOW)
-    print("Lane 3: False")
+    L3STS="False"
   else:
     #GPIO.output(LANE_1_LIGHT, GPIO.HIGH)
-    print("Lane 3: True")
+    L3STS="True"
     
   #if GPIO.input(LANE_4_SENSE) == 1:
   if LANE_4.value == 1:
-    print("Lane 4: False")
+    L4STS="False"
   else:
-    print("Lane 4: True")
+    L4STS="True"
+  print("Lane 3: " + L3STS + ", Lane 4: " + L4STS)
   time.sleep(0.2)
   
