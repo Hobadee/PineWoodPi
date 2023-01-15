706293bfc2b4440f3d876dc994dfebfaa7a4328d
Eric Kincl <eric@kincl.net>
2023-01-14 22:45:31 -0800

    Updated test code and timer mode

--- Testing.py
+++ Testing.py
@@ -3,15 +3,18 @@
 import RPi.GPIO as GPIO
 import time
 import board
+import os
 from gpiozero import Button, DigitalInputDevice
 
 GPIO.setmode(GPIO.BCM)
 
-LANE_1_SENSE=22
-LANE_2_SENSE=23
-LANE_3_SENSE=21
-LANE_4_SENSE=20
+LANE_1_SENSE=20
+LANE_2_SENSE=21
+LANE_3_SENSE=22
+LANE_4_SENSE=23
 
+LANE_1 = DigitalInputDevice(LANE_1_SENSE, True)
+LANE_2 = DigitalInputDevice(LANE_2_SENSE, True)
 LANE_3 = DigitalInputDevice(LANE_3_SENSE, True)
 #LANE_4 = Button(board.D23.id, True)
 LANE_4 = DigitalInputDevice(LANE_4_SENSE, True)
@@ -20,28 +23,41 @@ LANE_4 = DigitalInputDevice(LANE_4_SENSE, True)
 
 #GPIO.setup(LANE_1_SENSE, GPIO.IN)
 #GPIO.setup(LANE_2_SENSE, GPIO.IN)
-GPIO.setup(LANE_3_SENSE, GPIO.IN)
-GPIO.setup(LANE_4_SENSE, GPIO.IN)
+#GPIO.setup(LANE_3_SENSE, GPIO.IN)
+#GPIO.setup(LANE_4_SENSE, GPIO.IN)
 
 #GPIO.setup(LANE_1_LIGHT, GPIO.OUT)
 
+L1STS="False"
+L2STS="False"
 L3STS="False"
 L4STS="False"
 
 while True:
-  #if GPIO.input(LANE_3_SENSE) == 1:
-  if LANE_3.value == 1:
-    #GPIO.output(LANE_1_LIGHT, GPIO.LOW)
-    L3STS="False"
+  if LANE_1.value == 1:
+    L1STS="Car"
   else:
-    #GPIO.output(LANE_1_LIGHT, GPIO.HIGH)
-    L3STS="True"
+    L1STS="Clear"
+  
+  if LANE_2.value == 1:
+    L2STS="Car"
+  else:
+    L2STS="Clear"
     
-  #if GPIO.input(LANE_4_SENSE) == 1:
-  if LANE_4.value == 1:
-    L4STS="False"
+  if LANE_3.value == 1:
+    L3STS="Car"
   else:
-    L4STS="True"
-  print("Lane 3: " + L3STS + ", Lane 4: " + L4STS)
+    L3STS="Clear"
+    
+  if LANE_4.value == 1:
+    L4STS="Car"
+  else:
+    L4STS="Clear"
+
+  os.system('clear')
+  print("Lane 1: " + L1STS)
+  print("Lane 2: " + L2STS)
+  print("Lane 3: " + L3STS)
+  print("Lane 4: " + L4STS)
   time.sleep(0.2)
   
