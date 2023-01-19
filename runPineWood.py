fa25adfde2b88b457b79e7c48324477aaba27ce0
Eric Kincl <eric@kincl.net>
2023-01-18 18:22:18 -0800

    Cleaned up the starting function

--- runPineWood.py
+++ runPineWood.py
@@ -1,11 +1,38 @@
 #!/usr/bin/python3
 
-from PineWood import *
 
+from PineWood import *
+from log import *
+
+
+###################################
+#          Configuration          #
+###################################
+
+
+# Configure start button sensor
+startBtn = None
+
+# Configure lanes and sensors
 lanes = [{'no':1,'input':20},
         {'no':2,'input':21},
         {'no':3,'input':22},
         {'no':4,'input':23}]
 
-pinewood = pinewood(lanes)
+# Hold long should the race last before cars get a DNF?
+timeout = 5
+
+
+#################################################
+#          Create race object and race          #
+#################################################
+
+
+log = log()
+# DEBUG:
+log.setDisplayLevel('TRACE')
+
+pinewood = pinewood(lanes, startBtn, log)
+pinewood.setTimeout(5)
+
 pinewood.race()
