40417b01050307c6f5fad500dff24d94404e1ea2
Eric Kincl <eric@kincl.net>
2023-01-20 00:31:57 -0800

    Fixed LED ordering

--- runPineWood.py
+++ runPineWood.py
@@ -14,10 +14,10 @@ from log import *
 startBtn = 1
 
 # Configure lanes and sensors
-lanes = [{'no':1,'input':4,'rLED':22,'gLED':23},
-        {'no':2,'input':17,'rLED':24,'gLED':25},
-        {'no':3,'input':18,'rLED':5,'gLED':6},
-        {'no':4,'input':27,'rLED':20,'gLED':21}]
+lanes = [{'no':1,'input':4,'rLED':20,'gLED':21},
+        {'no':2,'input':17,'rLED':5,'gLED':6},
+        {'no':3,'input':18,'rLED':24,'gLED':25},
+        {'no':4,'input':27,'rLED':22,'gLED':23}]
 
 
 # Hold long should the race last before cars get a DNF?
